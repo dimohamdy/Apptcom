d069b24bd98afd21151de92fc63767964846785a
dimohamdy <dimo.hamdy@gmail.com>
2017-10-18 19:24:56 +0200

    Initial Commit

---
+++ Apptcom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Apptcom
+//
+//  Created by BinaryBoy on 10/18/17.
+//  Copyright © 2017 BinaryBoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
